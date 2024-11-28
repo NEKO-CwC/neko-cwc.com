538b5c9ff0ea52f92fa6fc3b050b33f9178c7fcb
NEKO-CwC <neeeeeeeeekooooooooo@gmail.com>
2024-11-28 16:21:59 +0800

    init component library add sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import { Sidebar as UISidebar, SidebarContent } from "@/components/ui/sidebar"
+
+const items = [
+    
+]
+
+const Sidebar = () => {
+    return (
+        <UISidebar side="right">
+            <SidebarContent />
+        </UISidebar>
+    )
+}
+
+export default Sidebar
